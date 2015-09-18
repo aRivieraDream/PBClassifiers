85206a49705895a806ea30ee12436d4abc709838
Pierce Young <808pierce@gmail.com>
2015-09-17 19:59:55 -0700

    add begining of vector building from mon night

--- README.md
+++ README.md
@@ -8,4 +8,11 @@ Software to determine keywords differentiating news articles within categories.
 * ~~Strip tab characters from every story.~~
 * generate tsv of stories-ending in 1 or 0
 * ~~process_text on tsv~~
-*  Compare vectors.
+* vectorizer
+  - convert all docs to a vector of frequencies where the vector is comprised of all non single-occurrence words in the corpus.
+  - **get ratio of occurrences of the keyword in that category vs the other (accuracy of that keyword of capturing docs in that category)**
+  - get **ratio of docs in that category that have that term (strength of keyword to capture that category)**
+  - **extra** get weight of that keyword in the document (tf)
+  - get uniqueness of that word in all documents (idf)
+* print ordered list of keywords with inter-cat freq * intra-cat freq
+*
--- pprocess.py
+++ pprocess.py
@@ -61,6 +61,12 @@ def make_features(data):
     X = vectorizer.fit_transform(corpus)
 """
 def make_features(data):
+    """
+    creates a matrix of stories with each row representing a story,
+    each column a word in the corpus, and each value the occurences.
+
+    another vector represents the category of each story-matching with the columns.
+    """
     corpus = [' '.join(v['processed_text']) for v in data.itervalues()]
     vectorizer = CountVectorizer(min_df=1)
     X = vectorizer.fit_transform(corpus)
@@ -69,17 +75,78 @@ def make_features(data):
     return X, y, vectorizer
 
 
-if __name__ == '__main__':
+def process_tsv(tsv):
+    """Takes a string location of tsv file
+    Col 1 (story)   | Col 2 (label)
+    -------------------------------
+    Story 1         | 1
+    Story 2         | 0
+
+    returns a dictionary with the format:
+    {Story 1:{processed_text:['word1', 'word2', 'word1', 'word3'], label:1}
+    Story 2:{processed_text:['word2', 'word2', 'word1', 'word5'], label:0}}
+    """
     # Note I stripped tab characters ('\t') before I dumped them in the tsv file.
-    # ' '.join(sample_string.split('\t'))
-    lines = open('data/sample_text_labeled.tsv', 'r').readlines()
+    lines = open(tsv, 'r').readlines()
     data = {}
     for i,line in enumerate(lines):
         tokens = line.split('\t')
         processed_text = process_text(tokens[0])
         data[i] = {'processed_text': processed_text,
                    'label': int(tokens[1].strip())}
-    print data
+    return data
+
+
+def map_doc(story, existing_map):
+    # get list of existing keywords, doc counts, word counts
+    keword_map = existing_map['keyword_map']
+    total_words = 0
+    # w1:[docs w/ word, times word appears] for this doc only
+    word_list = {}
+    ## grab word
+    next_word = 'placeholder' # replace with actual word in bucket
+    total_words = total_words + 1
+    prev = word_list.get(next_word)
+    counts = [1, 1]
+    if len(prev) == 2:
+        counts[0] = counts[0] + prev[0]
+        counts[1] = counts[1] + prev[1]
+    word_list[next_word] = counts
+    # add word list to rest of this category
+    # 
+    return total_words, word_freq, word_list
+
+
+def vectorize(data):
+    """
+    Takes a dictionary from process_tsv and returns
+    List of categories
+    List with doc count of each category
+    Dict of Dict of Lists:
+        {VC:{keword map: [w1, w2,...], doc occurences: [5, 10,...],
+            word occurences:[77, 23,...], total docs, total words},
+        PE{keyword map:[], doc occ:[], word occ:[], total docs,
+            total words},
+        ...
+        OT{keyword map:[all keywords from all categories], doc occ:[],
+        word occ:[], total docs, total words}}}
+    """
+    category_map = {'VC':{'keyword_map':[]}, 'PE':{}, 'M&A':{}, 'OT':{}}
+    vckm = []
+    makm = []
+    pekem = []
+    otkm = []
+
+
+
+if __name__ == '__main__':
+    tsv_name = 'data/sample_text_labeled.tsv'
+    # remove stopwords and lemmatize
+    data = process_tsv(tsv_name)
+    vectorize(data)
+
+
+    # ' '.join(sample_string.split('\t'))
 
     # open and check output with
     #with open('data/processed_text.pkl', 'r') as f:
